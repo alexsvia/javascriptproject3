b23c970c8db33c93c4dbb891f7d7b34d9625aa42
alexsvia <alexs@viacharacter.org>
2025-05-13 11:52:57 -0400

    Update Control Flow

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,52 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+
+// Boolean
+let isLoggedIn = true;
+let userMessage;
+
+// Check if user logged in
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+// Output user logged in
+console.log("User Message:", userMessage);
+
+// Declare variable, create string for subscriber
+let userType = "subscriber";
+let userCategory;
+
+// Swith statement to evaluate value (userType)
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+// Subscriber outpout
+console.log("User Category:", userCategory);
